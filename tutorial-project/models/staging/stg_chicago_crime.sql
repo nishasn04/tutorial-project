60298238b857cae64ab1eaf015eae42f27250464
Nisha Shetty <nishasn04@gmail.com>
2020-06-15 15:55:19 +0100

    transactions

--- tutorial-project/models/staging/stg_chicago_crime.sql
+++
@@ -1,5 +0,0 @@
-select
-
-*
-
-from {{ source('chicago_crime', 'crime')}}
--- tutorial-project/models/staging/stg_transactions.sql
+++ tutorial-project/models/staging/stg_transactions.sql
@@ -1,2 +1,4 @@
-select * from {{ source('transactions', 'transactions')}}
+{{ config(materialized='table') }}
+
+select * from {{ source('transactions', 'transactions') }}
 WHERE _fivetran_deleted = FALSE
